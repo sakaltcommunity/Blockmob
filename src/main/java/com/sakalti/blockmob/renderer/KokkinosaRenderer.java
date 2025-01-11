b9bfddc72107d458a6cb231d158edb2973d34c3b
Ingushe <167761978+Sakalt@users.noreply.github.com>
2025-01-11 11:09:27 +0900

    Update KokkinosaRenderer.java

--- src/main/java/com/sakalti/blockmob/renderer/KokkinosaRenderer.java
+++ src/main/java/com/sakalti/blockmob/renderer/KokkinosaRenderer.java
@@ -6,15 +6,15 @@ import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
 
-public class KokkinosaRenderer extends MobEntityRenderer<IgnitzeEntity, SquareModel<IgnitzeEntity>> {
+public class KokkinosaRenderer extends MobEntityRenderer<KokkinosaEntity, SquareModel<KokkinosaEntity>> {
     private static final Identifier TEXTURE = new Identifier("blockmob", "textures/entity/kokkinosa.png");
 
-    public IgnitzeRenderer(EntityRendererFactory.Context context) {
+    public KokkinosaRenderer(EntityRendererFactory.Context context) {
         super(context, new SquareModel<>(context.getPart(SquareModel.LAYER_LOCATION)), 0.5f);
     }
 
     @Override
-    public Identifier getTexture(IgnitzeEntity entity) {
+    public Identifier getTexture(KokkinosaEntity entity) {
         return TEXTURE;
     }
 }
